3b39c58817b934cf89093be7d0ad096d5e49bd9f
LightBrigade <lightbrigadeofficial@gmail.com>
2024-07-14 01:04:57 +0530

    Add files via upload
    commit 3225kTv25LpPB0
    112.445.8755.14

---
+++ commands.js
@@ -0,0 +1,65 @@
+var youtube = "https://www.youtube.com/@LightBrigadeGlobal";
+var twitter = "#";
+var password = "lorem ipsum dolor sit amet";
+var linkedin = "#";
+var email = 'mailto:hyeonteam23@gmail.com'; //personal email I'd for trial purpose//
+var lorem = '#';
+var ipsum = 'Nothing';
+
+whois = [
+  "<br>",
+  "Hey there,",
+  "Lorem ipsum dolor sit amet",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "#",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/lightbrigade' + "</a>",
+  'twitter        <a href="' + ipsum + '" target="_blank">#' + '</a>',
+  'linkedin       <a href="' + lorem + '" target="_blank">#' + "</a>",
+
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin', //trail commit//
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">About</span>          Abouts',
+  '<span class="command">lorem ipsum</span>         #',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">#</span>         #',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          EMAIL',
+  '<span class="command">cls</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="color2">Hi there,</span>',
+ 
+  "<span class=\"color2\">I'm Vandam Singha</span>", 
+  "<span class=\"color2\">Welcome to VAND-LINUX 1.0 LTS</span>",
+  "<span class=\"color2\"> >>For a list of available commands, type</span> <span class=\"color2\">'help'</span><span class=\"color2\">.</span>",
+];
